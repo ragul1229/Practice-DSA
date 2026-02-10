ebbb0aa62e510ee0d7c703c0737ccbf482f95d43
Ragul J <jsragul1229@gmail.com>
2026-02-10 09:11:00 +0530

    Added threesun

---
+++ Arrays/ThreeSum.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class ThreeSum {
+    public static void main(String args[]){
+        int[] arr = {-1,0,1,2,-1,-4};
+        Arrays.sort(arr);
+        Set<List<Integer>> set = new HashSet<>();
+        for(int i = 0 ; i < arr.length-2 ; i++){
+            int j = i+1;
+            int k = arr.length - 1;
+            while(j<k){
+                int sum = arr[i] + arr[j] + arr[k];
+                if(sum == 0){
+                    set.add(Arrays.asList(arr[i],arr[j],arr[k]));
+                    j++;
+                    k--;
+                }else if(sum < 0){
+                    j++;
+                }else{
+                    k--;
+                }
+            }
+        }
+        System.out.print(set);
+    }
+}
